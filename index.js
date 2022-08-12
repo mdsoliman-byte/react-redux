1d92a251357fccb1e0f5e6f1cd7ac8cffa367a72
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-08-12 21:05:11 +0600

    create a action

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+
 //Create A State 
 const countState = {
     count: 0,
 };
+const userState = {
+    users: [{ name: "Shakil" }]
+}
 
 
+//Create A  Action
 
-//Create A  Action 
+const actionCreator = () => {
+    return {
+        type: INCREMENT
+    }
+}
+// Increment Counter
+// Decrement Counter 
+
 
 
 
